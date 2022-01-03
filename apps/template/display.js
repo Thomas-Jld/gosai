846b3929b2d0d0b8498b1ba74c505453a897a4eb
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-03 22:21:43 +0100

    Corrected mistakes and more simple

--- apps/template/display.js
+++ apps/template/display.js
@@ -1,12 +1,18 @@
-export const template = ( sketch ) => {
+export const template = new p5(( sketch ) => {
     sketch.name = "template"
+    sketch.z_index = 0
+    sketch.activated = false
 
-    sketch.setup = (width, height, socket) => {
-        sketch.selfCanvas = sketch.createCanvas(width, height).position(0, 0);
+    sketch.set = (width, height, socket) => {
+        sketch.selfCanvas = sketch.createCanvas(width, height).position(0, 0).style("z-index", sketch.z_index);
 
         socket.on("template", (data) => {
 
         });
+    }
+
+    sketch.windowResized = () => {
+        sketch.resizeCanvas(windowWidth, windowHeight);
     }
 
     sketch.update = () => {}
@@ -14,4 +20,4 @@ export const template = ( sketch ) => {
     sketch.show = () => {
         sketch.clear();
     }
-}
+});
--- apps/template/processing.py
+++ apps/template/processing.py
@@ -1,45 +1,20 @@
-import threading
+from apps.application import BaseApplication
 
-class Application(threading.Thread):
-    """Template class for applications"""
+
+class Application(BaseApplication):
+    """SLR"""
 
     def __init__(self, name, hal, server, manager):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.hal = hal
-        self.server = server
-        self.manager = manager
+        super().__init__(name, hal, server, manager)
 
-        self.requires = [] # Select what driver you want to use
+        # * Example
+        # self.requires = {"name of the driver": ["data"]}
 
-        self.data = {} # data that is sent to the js script
-
-        self.started = False
-
-        @self.server.sio.on(f"update_{self.name}")
-        def _send_data(*_) -> None:
-            """Sends data to the client upon request"""
-            pass
 
     def listener(self, source, event):
-        """Gets notified when some data of a driver is updated"""
-        if source not in self.requires:
-            self.hal.log(f"{self.name}: subscribed to an unrequested event.")
-            return
-        if event not in self.requires[source]:
-            self.hal.log(f"{self.name}: not subscrbed to {event} from {source}")
-            return
+        super().listener(source, event)
 
-        # Write your code here (what to do when the data is recieved)
-
-
-    def stop(self):
-        """Stops the application"""
-        self.started = False
-
-    def run(self):
-        """Thread that runs the application"""
-        self.started = True
-
-    def __str__(self):
-        return str(self.name)
+        # * Example
+        # if self.started and source == "a_driver" and event == "some_data":
+        #     self.data = self.hal.drivers["a_driver"].some_data
+        #     self.server.send_data(self.name, self.data)
