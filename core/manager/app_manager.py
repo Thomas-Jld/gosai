14077f7d8822825f4a226bc20c7f1fa313b1f0c2
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-07 16:02:08 +0100

    Better logging

--- core/manager/app_manager.py
+++ core/manager/app_manager.py
@@ -1,5 +1,7 @@
+import datetime
 import os
 
+LOGS_PATH = "core/hal/logs"
 
 class AppManager:
     """Manages the apps"""
@@ -29,9 +31,6 @@ class AppManager:
     def list_applications(self) -> list:
         """Lists all available applications"""
 
-        # self.hal.log(
-        #     f"{self.name}: Available applications are {', '.join(self.available_apps)}"
-        # )
         return self.available_apps
 
     def list_started_applications(self) -> list:
@@ -48,64 +47,94 @@ class AppManager:
             if app_name not in self.started_apps
         ]
 
-    def start(self, app_name: str) -> None:
+    def start(self, app_name: str) -> bool:
         """Starts an application"""
 
-        if app_name in self.available_apps:
-            try:
-                # Import the python app
-                app = __import__(
-                    f"apps.{app_name}.processing", fromlist=[None]
-                ).Application(app_name, self.hal, self.server, self)
+        if app_name not in self.available_apps:
+            self.log(f"Unknown application '{app_name}'", 3)
+            return False
 
-                # Start the required drivers and subscribe to the required events
-                for driver_name in app.requires:
-                    if not self.hal.drivers[driver_name].started:
-                        self.hal.drivers[driver_name].start()
-                    for event in app.requires[driver_name]:
-                        self.hal.drivers[driver_name].register(app, event)
+        if app_name in self.started_apps:
+            self.log(f"Application '{app_name}' already started", 3)
+            return False
 
-                # Start the python app
-                app.start()
+        try:
+            # Import the python app
+            app = __import__(
+                f"apps.{app_name}.processing", fromlist=[None]
+            ).Application(app_name, self.hal, self.server, self)
 
-                # Start the js app
-                self.server.send_data(
-                    "start_application", {"application_name": app_name}
-                )
+            # Start the required drivers and subscribe to the required events
+            for driver_name in app.requires:
+                self.hal.start_driver(driver_name)
+                for event in app.requires[driver_name]:
+                    self.hal.register_to_driver(driver_name, app, event)
 
-                # Store the app as a started app
-                self.started_apps[app_name] = app
-                self.hal.log(f"{self.name}: Started application '{app_name}'")
+            # Start the python app
+            app.start()
 
-            except Exception as e:
-                self.hal.log(
-                    f"{self.name}: Failed to start application '{app_name}': {e}"
-                )
-        else:
-            self.hal.log(f"{self.name}: Unknown application '{app_name}'")
+            # Start the js app
+            self.server.send_data(
+                "start_application", {"application_name": app_name}
+            )
 
-    def stop(self, app_name: str) -> None:
+            # Store the app as a started app
+            self.started_apps[app_name] = app
+            self.log(f"Started application '{app_name}'", 2)
+
+        except Exception as e:
+            self.log(
+                f"Failed to start application '{app_name}': {e}", 4
+            )
+            return False
+
+        return True
+
+
+    def stop(self, app_name: str) -> bool:
         """Stops an application"""
 
-        if app_name in self.available_apps:
-            if app_name in self.started_apps:
-                try:
-                    # Stop the python app
-                    self.started_apps[app_name].stop()
+        if app_name not in self.available_apps:
+            self.log(f"Unknown application '{app_name}'", 3)
+            return False
 
-                    # Stop the js app
-                    self.server.send_data(
-                        "stop_application", {"application_name": app_name}
+        if app_name not in self.started_apps:
+            self.log(f"Application '{app_name}' not started", 3)
+            return False
+
+        try:
+            app = self.started_apps[app_name]
+            for driver_name in app.requires:
+                for event in app.requires[driver_name]:
+                    self.hal.unregister_from_driver(
+                        driver_name, app, event
                     )
 
-                    del self.started_apps[app_name]
+            # Stop the python app
+            app.stop()
 
-                    self.hal.log(f"{self.name}: Stopped application '{app_name}'")
-                except Exception as e:
-                    self.hal.log(
-                        f"{self.name}: Failed to stop application '{app_name}': {e}"
-                    )
-            else:
-                self.hal.log(f"{self.name}: Application '{app_name}' not started")
-        else:
-            self.hal.log(f"{self.name}: Unknown application '{app_name}'")
+            # Stop the js app
+            self.server.send_data(
+                "stop_application", {"application_name": app_name}
+            )
+
+            del self.started_apps[app_name]
+            self.log(f"Stopped application '{app_name}'", 2)
+
+        except Exception as e:
+            self.log(
+                f"Failed to stop application '{app_name}': {e}", 4
+            )
+            return False
+
+        return True
+
+    def log(self, message, level=1):
+        """Logs a message"""
+        if level >= int(os.environ["LOG_LEVEL"]):
+            print(f"{self.name}: {message}")
+
+        with open(f"{LOGS_PATH}/app_manager.log", "a+") as log:
+            log.write(
+                f"{datetime.datetime.now().strftime('%b-%d-%G-%I:%M:%S%p')} : {message}\n"
+            )
