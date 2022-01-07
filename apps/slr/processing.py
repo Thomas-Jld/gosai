bbb64bd8f8740da83f120227e0ec87bc53c004e8
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-07 15:58:23 +0100

    Preparing for slr demo

---
+++ apps/slr/processing.py
@@ -0,0 +1,17 @@
+from apps.application import BaseApplication
+
+
+class Application(BaseApplication):
+    """SLR"""
+
+    def __init__(self, name, hal, server, manager):
+        super().__init__(name, hal, server, manager)
+        # self.requires = {"pose": ["mirrored_data"]}
+
+
+    def listener(self, source, event):
+        super().listener(source, event)
+
+        # if self.started and source == "pose" and event == "mirrored_data":
+        #     self.data = self.hal.drivers["pose"].mirrored_data
+        #     self.server.send_data(self.name, self.data)
