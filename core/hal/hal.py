81d1e572d2bec203e9ff34fd35589e6c7eadcfb2
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-07 16:02:22 +0100

    Interfacing for access to drivers

--- core/hal/hal.py
+++ core/hal/hal.py
@@ -2,46 +2,118 @@
 
 import datetime
 import os
-import sys
-import threading
-import time
+from typing import Any
 
-CURR_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(CURR_DIR)
+DRIVERS_PATH = "core/hal/drivers"
+LOGS_PATH = "core/hal/logs"
 
 
 class HardwareAbstractionLayer:
-    """Main interface to access the drivers"""
+    """
+    Main interface to access the drivers
+    Should be used by the apps to access the drivers
+    """
 
     def __init__(self):
 
+        self.name = "hal"
+        self.log(f"Starting {self.name}")
         self.available_drivers = [
             f.path.split("/")[-1]
-            for f in os.scandir(f"{CURR_DIR}/drivers")
+            for f in os.scandir(DRIVERS_PATH)
             if f.is_dir() and f.path.split("/")[-1] != "__pycache__"
         ]
 
         self.drivers = {}
 
         for driver_name in self.available_drivers:
-            driver = __import__(
-                f"drivers.{driver_name}.{driver_name}", fromlist=[None]
-            ).Driver(driver_name, self)
-            self.drivers[driver_name] = driver
+            if driver_name not in self.drivers:
+                self.init_driver(driver_name)
 
-    def load_driver(self, driver_name):
+    def init_driver(self, driver_name: str) -> bool:
+        """
+        Initializes a driver
+        """
         if driver_name not in self.available_drivers:
-            self.log(f"{driver_name} is not a valid driver")
+            self.log(f"Cannot init: {driver_name} is not a valid driver", 3)
             return False
 
-        self.drivers[driver_name] = self.drivers[driver_name].load()
+        driver = __import__(
+            f"{DRIVERS_PATH.replace('/', '.')}.{driver_name}.{driver_name}",
+            fromlist=[None],
+        ).Driver(driver_name, self)
+        self.drivers[driver_name] = driver
+
         return True
+
+    def start_driver(self, driver_name: str) -> bool:
+        """
+        Starts a driver and all its dependecies
+        """
+        if driver_name not in self.available_drivers:
+            self.log(f"{driver_name} is not a valid driver", 3)
+            return False
+
+        driver = self.drivers[driver_name]
+
+        if not driver.started or driver.paused:
+            for req_driver_name in driver.requires:
+                if req_driver_name not in self.available_drivers:
+                    self.log(
+                        f"Driver '{req_driver_name}' is required \
+                            by '{driver.name}' but is not valid",
+                        3,
+                    )
+                    return False
+                self.start_driver(req_driver_name)
+            if not driver.started:
+                driver.start()
+            if driver.paused:
+                driver.resume()
+
+        return True
+
+    def register_to_driver(self, driver_name: str, entity: str, event: str) -> bool:
+        """
+        Registers an entity to an event of a driver
+        """
+        if driver_name not in self.drivers:
+            if driver_name not in self.available_drivers:
+                self.log(f"Cannot register: {driver_name} is not a valid driver", 3)
+                return False
+            self.init_driver(driver_name)
+
+        self.drivers[driver_name].register(entity, event)
+
+        return True
+
+    def unregister_from_driver(self, driver_name: str, entity: str, event: str) -> bool:
+        """
+        Unregisters an entity from an event of a driver
+        """
+        if driver_name not in self.drivers:
+            self.log(f"Cannot unregister: {driver_name} is not a valid driver", 3)
+            return False
+
+        self.drivers[driver_name].unregister(entity, event)
+
+        return True
+
+    def get_driver_event_data(self, driver_name: str, event: str) -> Any:
+        """
+        Returns the data of an event of a driver
+        """
+        if driver_name not in self.drivers:
+            self.log(f"Cannot get data: {driver_name} is not a valid driver", 3)
+            return False
+
+        return self.drivers[driver_name].get_event_data(event)
 
     def get_started_drivers(self) -> str:
         """Returns a list of drivers that are started"""
         started_drivers = []
         for driver_name, driver in self.drivers.items():
-            if driver.started:
+            if driver.started and not driver.paused:
                 started_drivers.append(driver_name)
         return started_drivers
 
@@ -49,7 +121,7 @@ class HardwareAbstractionLayer:
         """Returns a list of drivers that are stopped"""
         stopped_drivers = []
         for driver_name, driver in self.drivers.items():
-            if not driver.started:
+            if not driver.started or driver.paused:
                 stopped_drivers.append(driver_name)
         return stopped_drivers
 
@@ -57,14 +129,15 @@ class HardwareAbstractionLayer:
         """Returns a list of available drivers"""
         return self.available_drivers
 
-    def log(self, message):
+    def log(self, message, level=1):
         """
         Logs every things (for now in the console)
         TODO: create a temporary log file
         """
-        print(message)
+        if level >= int(os.environ["LOG_LEVEL"]):
+            print(f"{self.name}: {message}")
 
-        with open("core/hal/logs/hal.log", "a+") as log:
+        with open(f"{LOGS_PATH}/{self.name}.log", "a+") as log:
             log.write(
                 f"{datetime.datetime.now().strftime('%b-%d-%G-%I:%M:%S%p')} : {message}\n"
             )
