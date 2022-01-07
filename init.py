3d935a9b93b94d96f38e20d9a9a852119050182a
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-07 15:55:30 +0100

    Adding log level and removing useless app listing

--- init.py
+++ init.py
@@ -1,14 +1,17 @@
+import os
+
 from core.console.console import Console
 from core.hal.hal import HardwareAbstractionLayer
 from core.manager.app_manager import AppManager
 from core.server.server import Server, start_chrome
+
+os.environ["LOG_LEVEL"] = "2"
 
 hal = HardwareAbstractionLayer()
 server = Server(hal)
 server.start()
 
 app_manager = AppManager(hal, server)
-app_manager.list_applications()
 app_manager.start("menu")
 
 start_chrome()
