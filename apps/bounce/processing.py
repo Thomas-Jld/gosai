cdfe27a87bdc0f85240719d8b5b1468dcdbfa87a
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-11 00:26:40 +0100

    New app: Make it bounce !

---
+++ apps/bounce/processing.py
@@ -0,0 +1,20 @@
+from apps.application import BaseApplication
+
+
+class Application(BaseApplication):
+    """Bounce"""
+
+    def __init__(self, name, hal, server, manager):
+        super().__init__(name, hal, server, manager)
+        self.requires["pose_to_mirror"] = ["mirrored_data"]
+
+    def listener(self, source, event):
+        super().listener(source, event)
+
+        if source == "pose_to_mirror" and event == "mirrored_data":
+            self.data = self.hal.get_driver_event_data("pose_to_mirror", "mirrored_data")
+            self.data = {
+                "left_hand_pose": self.data["left_hand_pose"],
+                "right_hand_pose": self.data["right_hand_pose"]
+            }
+            self.server.send_data(self.name, self.data)
