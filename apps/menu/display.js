8911981ae1b3e20e77ec2478694903a55f2001cd
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-17 14:30:52 +0100

    Ignoring slr once again (for the demo)

--- apps/menu/display.js
+++ apps/menu/display.js
@@ -49,7 +49,8 @@ export const menu = new p5((sketch) => {
             sketch.menu.remove_all_from(0);
             for (let i = 0; i < data["started"].length; i++) {
                 if (
-                    data["started"][i] != "menu"
+                    data["started"][i] != "menu" &&
+                    data["started"][i] != "slr"
                 ) {
                     sketch.menu.add_application(0, data["started"][i], true);
                 }
@@ -57,7 +58,8 @@ export const menu = new p5((sketch) => {
 
             for (let i = 0; i < data["stopped"].length; i++) {
                 if (
-                    data["stopped"][i] != "menu"
+                    data["stopped"][i] != "menu" &&
+                    data["stopped"][i] != "slr"
                 ) {
                     sketch.menu.add_application(0, data["stopped"][i], false);
                 }
