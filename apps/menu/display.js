4889f99fb3caacd00e3a681b39c37ac9bddaca50
Thomas <thomas.juldo@edu.devinci.fr>
2022-01-17 11:58:59 +0100

    Unlocking slr

--- apps/menu/display.js
+++ apps/menu/display.js
@@ -1,12 +1,12 @@
-import { Face } from "./components/face.js"
-import { Hand } from "./components/hand.js"
-import { Body } from "./components/body.js"
-import { Menu } from "./components/menu2.js"
+import { Face } from "./components/face.js";
+import { Hand } from "./components/hand.js";
+import { Body } from "./components/body.js";
+import { Menu } from "./components/menu2.js";
 
-export const menu = new p5(( sketch ) => {
-    sketch.name = "menu"
-    sketch.z_index = 5
-    sketch.activated = false
+export const menu = new p5((sketch) => {
+    sketch.name = "menu";
+    sketch.z_index = 5;
+    sketch.activated = false;
 
     sketch.face;
     sketch.body;
@@ -15,14 +15,16 @@ export const menu = new p5(( sketch ) => {
     sketch.menu;
 
     sketch.set = (width, height, socket) => {
+        sketch.face = new Face("face");
+        sketch.body = new Body("body");
+        sketch.right_hand = new Hand("right_hand");
+        sketch.left_hand = new Hand("left_hand");
+        sketch.menu = new Menu(0, 0, 150, sketch);
 
-        sketch.face = new Face("face")
-        sketch.body = new Body("body")
-        sketch.right_hand = new Hand("right_hand")
-        sketch.left_hand = new Hand("left_hand")
-        sketch.menu = new Menu(0, 0, 150, sketch)
-
-        sketch.selfCanvas = sketch.createCanvas(width, height).position(0, 0).style("z-index", sketch.z_index);
+        sketch.selfCanvas = sketch
+            .createCanvas(width, height)
+            .position(0, 0)
+            .style("z-index", sketch.z_index);
 
         sketch.angleMode(RADIANS);
         sketch.textAlign(CENTER, CENTER);
@@ -30,35 +32,44 @@ export const menu = new p5(( sketch ) => {
         sketch.imageMode(CENTER);
 
         socket.on(sketch.name, (data) => {
-            sketch.face.update_data(data["face_mesh"])
-            sketch.body.update_data(data["body_pose"])
-            sketch.right_hand.update_data(data["right_hand_pose"], data["right_hand_sign"])
-            sketch.left_hand.update_data(data["left_hand_pose"], data["left_hand_sign"])
-            sketch.menu.update_data(sketch.left_hand, sketch.right_hand)
+            sketch.face.update_data(data["face_mesh"]);
+            sketch.body.update_data(data["body_pose"]);
+            sketch.right_hand.update_data(
+                data["right_hand_pose"],
+                data["right_hand_sign"]
+            );
+            sketch.left_hand.update_data(
+                data["left_hand_pose"],
+                data["left_hand_sign"]
+            );
+            sketch.menu.update_data(sketch.left_hand, sketch.right_hand);
         });
 
         socket.on("list_applications", (data) => {
             sketch.menu.remove_all_from(0);
-            for(let i = 0; i < data["started"].length; i++) {
-                if(data["started"][i] != "menu" && data["started"][i] != "slr") {
-                    sketch.menu.add_application(0, data["started"][i], true)
+            for (let i = 0; i < data["started"].length; i++) {
+                if (
+                    data["started"][i] != "menu"
+                ) {
+                    sketch.menu.add_application(0, data["started"][i], true);
                 }
             }
 
-            for(let i = 0; i < data["stopped"].length; i++) {
-                if(data["stopped"][i] != "menu" && data["stopped"][i] != "slr") {
-                    sketch.menu.add_application(0, data["stopped"][i], false)
+            for (let i = 0; i < data["stopped"].length; i++) {
+                if (
+                    data["stopped"][i] != "menu"
+                ) {
+                    sketch.menu.add_application(0, data["stopped"][i], false);
                 }
             }
-
         });
 
         sketch.emit = (name, data) => {
             socket.emit(name, data);
-        }
+        };
 
-        sketch.activated = true
-    }
+        sketch.activated = true;
+    };
 
     sketch.resume = () => {};
 
@@ -66,7 +77,7 @@ export const menu = new p5(( sketch ) => {
 
     sketch.windowResized = () => {
         sketch.resizeCanvas(windowWidth, windowHeight);
-    }
+    };
 
     sketch.update = () => {
         sketch.face.update();
@@ -74,7 +85,7 @@ export const menu = new p5(( sketch ) => {
         sketch.right_hand.update();
         sketch.left_hand.update();
         sketch.menu.update();
-    }
+    };
 
     sketch.show = () => {
         sketch.clear();
@@ -83,5 +94,5 @@ export const menu = new p5(( sketch ) => {
         sketch.right_hand.show(sketch);
         sketch.left_hand.show(sketch);
         sketch.menu.show(sketch);
-    }
+    };
 });
